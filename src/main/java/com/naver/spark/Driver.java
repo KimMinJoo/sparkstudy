51188380079e22b96a96d489c7de55a08b4e9237
kim-minjoo <kim.minjoo@nhn.com>
2017-02-15 15:47:03 +0900

    Project생성 첫 커밋

---
+++ src/main/java/com/naver/spark/Driver.java
@@ -0,0 +1,16 @@
+/*
+ *@(#)Driver.java 2017.02.15
+ *
+ * Copyright 2017 NHN Corp. All rights Reserved.
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.naver.spark;
+
+/**
+ * spark 프로그램을 돌릴 Driver클래스
+ *
+ * @author kim.minjoo
+ */
+public class Driver {
+}
