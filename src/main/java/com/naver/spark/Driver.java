827bed4ce5ff6999999790ca3ed098a6b654ce36
kim-minjoo <kim.minjoo@nhn.com>
2017-02-15 15:54:41 +0900

    스파크 초기화 하기

--- src/main/java/com/naver/spark/Driver.java
+++ src/main/java/com/naver/spark/Driver.java
@@ -7,10 +7,17 @@
 
 package com.naver.spark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * spark 프로그램을 돌릴 Driver클래스
  *
  * @author kim.minjoo
  */
 public class Driver {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+	}
 }
